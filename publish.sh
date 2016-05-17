5dc41e611c0d051f6e52b2450c709f19cfaf7ae4
Sercan Değirmenci <degirmencisercan@gmail.com>
2016-05-17 17:24:37 +0300

    fix publish.sh script

--- publish.sh
+++ publish.sh
@@ -7,16 +7,8 @@ gversion=$(jq .version --raw-output otsimo.json)
 gname=$(jq .unique_name --raw-output otsimo.json)
 
 
-if [ "$1" = "" ];then
-    export OTSIMOCTL_CATALOG=services.otsimo.xyz:18857
-    export OTSIMOCTL_REGISTRY=registry.otsimo.xyz:18852
-    export OTSIMOCTL_UPLOAD=https://registry.otsimo.xyz:18851
-    export OTSIMOCTL_ACCOUNTS=https://accounts.otsimo.xyz
-elif [ "$1" = "prod" ];then
-    export OTSIMOCTL_CATALOG=services.otsimo.com:18857
-    export OTSIMOCTL_REGISTRY=registry.otsimo.com:18852
-    export OTSIMOCTL_UPLOAD=https://registry.otsimo.com:18851
-    export OTSIMOCTL_ACCOUNTS=https://accounts.otsimo.com
+if [ "$1" = "staging" ];then
+    export OTSIMOCTL_DISCOVERY=services.otsimo.xyz:30862
 fi
 
 otsimoctl game publish
